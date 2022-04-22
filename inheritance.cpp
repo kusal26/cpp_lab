ab7beaf314d10a9551f2d066d7b78ebd81b30fe8
decodex <tkusal965@gmail.com>
2022-04-22 19:38:16 +0545

    hello

---
+++ inheritance.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class grandfather
+{
+    protected:
+    string car;
+    int land;
+    public:
+    void grand_father_property(string a,int b)
+    {
+        car=a;
+        land=b;
+        
+    }
+    void display1()
+    {
+        cout<<"car= "<<car<<"\n"<<"land= "<<land<<endl;
+    }
+
+};
+class father: public grandfather
+{
+    protected:
+    string bike;
+    public:
+    void father_property(string c)
+    {
+        bike=c;
+    }
+    void display2()
+    {
+        cout<<"Father has "<<bike<<" bike"<<endl;
+    }
+
+};
+class son : public father
+{
+    string car;
+    public:
+    void son_property(string d)
+    {
+        car=d;
+    }
+    void display3()
+    {
+        cout<<"son has "<<car<<" car">>endl;
+    }
+
+
+};
+class grandson : public son
+{
+
+};
+int main()
+{
+    grandson 
+    g.grand_father_property("nano",10);
+    g.display1();
+    g.father_property("yamaha");
+    g.display2();
+    g.son_property("ferari");
+    g.display3();
+
+}
